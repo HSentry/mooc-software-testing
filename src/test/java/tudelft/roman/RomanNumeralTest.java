142afafc895dc8ab728f859a9a71e3f218c81b5d
Jaime Minaya <jaime.minaya@utec.edu.pe>
2021-03-24 00:12:05 -0500

    Lesson 3

--- src/test/java/tudelft/roman/RomanNumeralTest.java
+++ src/test/java/tudelft/roman/RomanNumeralTest.java
@@ -1,36 +1,47 @@
 package tudelft.roman;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class RomanNumeralTest {
 
+    // Crea el objeto roman para que pueda ser usado en todas las pruebas sin necesidad de crearlo en cada uno
+    private RomanNumeral roman;
 
+    @BeforeEach
+    public void initialize(){
+        this.roman=new RomanNumeral();
+    }
+
+    // Inicio de pruebas
     @Test
     public void singleNumber() {
-        RomanNumeral roman = new RomanNumeral();
         int result = roman.convert("I");
         Assertions.assertEquals(1, result);
     }
 
     @Test
     public void numberWithManyDigits() {
-        RomanNumeral roman = new RomanNumeral();
         int result = roman.convert("VIII");
         Assertions.assertEquals(8, result);
     }
 
     @Test
     public void numberWithSubtractiveNotation() {
-        RomanNumeral roman = new RomanNumeral();
         int result = roman.convert("IV");
         Assertions.assertEquals(4, result);
     }
 
     @Test
     public void numberWithAndWithoutSubtractiveNotation() {
-        RomanNumeral roman = new RomanNumeral();
         int result = roman.convert("XLIV");
         Assertions.assertEquals(44, result);
+    }
+
+    @Test
+    public void pruebadetexto(){
+        int result = roman.convert("VI");
+        Assertions.assertEquals(6,result);
     }
 }
