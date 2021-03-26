da94e30e1f2cedc8fbcdb00201481b9e094b09a6
Jaime Minaya <jaime.minaya@utec.edu.pe>
2021-03-25 20:10:57 -0500

    Quiz Topics 1 & 2

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,36 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import tudelft.mirror.Mirror;
+
+
 public class GHappyTest {
+    // Crea el objeto happy para que pueda ser usado en todas las pruebas sin necesidad de crearlo en cada uno
+    private GHappy happy;
+
+    @BeforeEach
+    public void initialize(){
+        this.happy=new GHappy();
+    }
+
+    // Inicio de pruebas
+    @Test
+    public void test1() {
+        boolean result = happy.gHappy("xggx");
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void test2() {
+        boolean result = happy.gHappy("xgx");
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    public void test3() {
+        boolean result = happy.gHappy("xggxgx");
+        Assertions.assertFalse(result);
+    }
 }
--- src/test/java/tudelft/mirror/MirrorTest.java
+++ src/test/java/tudelft/mirror/MirrorTest.java
@@ -1,4 +1,35 @@
 package tudelft.mirror;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class MirrorTest {
+    // Crea el objeto mirror para que pueda ser usado en todas las pruebas sin necesidad de crearlo en cada uno
+    private Mirror mirror;
+
+    @BeforeEach
+    public void initialize(){
+        this.mirror=new Mirror();
+    }
+
+    // Inicio de pruebas
+    @Test
+    public void test1() {
+        String result = mirror.mirrorEnds("abXYZba");
+        Assertions.assertEquals("ab", result);
+    }
+
+    @Test
+    public void test2() {
+        String result = mirror.mirrorEnds("abca");
+        Assertions.assertEquals("a", result);
+    }
+
+    @Test
+    public void test3() {
+        String result = mirror.mirrorEnds("aba");
+        Assertions.assertEquals("aba", result);
+    }
+
 }
